53640fc91a75edacf7abf1eb65c5ecf8f4358593
Matthew Sperry <mbsperry@users.noreply.github.com>
2023-03-25 00:40:51 +0000

    Initial commit - includes partial static layout

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
-import './App.css';
+// import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form, InputGroup, Container, Button, Card, Stack } from "react-bootstrap";
+
+function SprinklrSelect() {
+  return (
+    <InputGroup>
+      <InputGroup.Text>Select Sprinklr</InputGroup.Text>
+        <Form.Select>
+          <option>Select sprinklr</option>
+       </Form.Select>
+    </InputGroup>
+  )
+}
+
+function DurationInput() {
+  return (
+   <InputGroup>
+      <Form.Control type="text"></Form.Control>
+      <Button variant="success">Activate!</Button>
+    </InputGroup>
+  )
+}
+
+function InputCard() {
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <Stack gap="2">
+            <SprinklrSelect />
+            <DurationInput />
+          </Stack>
+        </Card.Body>
+      </Card>
+    </>
+  )
+}
+
+function StatusCard() {
+  return (
+    <>
+      <Card bg="info">
+        <Card.Body>
+          System disabled
+        </Card.Body>
+      </Card>
+    </>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <Container>
+      <h1>###iSprinklr###</h1>
+      <p>React based Sprinklr control</p>
+      <Stack gap="2">
+        <InputCard />
+        <StatusCard />
+      </Stack>
+    
+    </Container>
   );
 }
 
