8e07777406476c4a7576ae8c471bc181c88bc249
Matthew Sperry <mbsperry@users.noreply.github.com>
2023-03-28 15:59:14 +0000

    Basic layout complete, input validation complete

--- src/App.js
+++ src/App.js
@@ -1,37 +1,92 @@
-// import './App.css';
+ import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 import { Form, InputGroup, Container, Button, Card, Stack } from "react-bootstrap";
 
-function SprinklrSelect() {
+const Sprinklers = { "1": "East", "2": "West", "3": "Backyard" }
+const BLUE = "#cfe2ff";
+const GREEN = "#d1e7dd";
+const RED = "#f8d7da";
+
+function SprinklrSelect({onChange}) {
+  function MakeList() {
+    const options = [];
+    for (const key in Sprinklers) {
+      if (Sprinklers.hasOwnProperty(key)){
+        options.push(<option value={key}>{Sprinklers[key]}</option>);
+      }
+    }
+    return (
+      <>
+        {options}
+      </>
+    )
+  }
+
   return (
     <InputGroup>
       <InputGroup.Text>Select Sprinklr</InputGroup.Text>
-        <Form.Select>
-          <option>Select sprinklr</option>
+        <Form.Select onChange={(e) => onChange(e)}>
+          <option value="0">None</option>
+          {MakeList()}
        </Form.Select>
     </InputGroup>
   )
 }
 
-function DurationInput() {
+function DurationInput({sprinklr}) {
+  const [validated, setValidated] = useState(false);
+  function handleSubmit(e) {
+    if (e.target.checkValidity() === false) { 
+      e.preventDefault(); 
+      setValidated(true);
+      return 
+    };
+    e.preventDefault();
+    console.log("Sumbitted");
+    console.log(e.target)
+  }
+  if (sprinklr < 1) {
+    return <></>
+  }
   return (
-   <InputGroup>
-      <Form.Control type="text"></Form.Control>
-      <Button variant="success">Activate!</Button>
-    </InputGroup>
+    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Form.Control required type="number" min="1" step="1" max="60" placeholder='Duration in whole minutes'></Form.Control>
+      <Form.Control.Feedback type="invalid">Please enter duration in whole minutes only. Max 60 min.</Form.Control.Feedback>
+      <Button type="submit" className="mt-2">Activate!</Button>
+    </Form>
   )
 }
 
 function InputCard() {
+  const [sprinklr, setSprinklr] = useState("0");
+  
+  function onSprinklrChange(e) {
+    console.log(e.target.value);
+    setSprinklr(e.target.value);
+  }
+
   return (
     <>
       <Card>
         <Card.Body>
           <Stack gap="2">
-            <SprinklrSelect />
-            <DurationInput />
+            <SprinklrSelect onChange={onSprinklrChange} />
+            <DurationInput sprinklr={sprinklr}/>
           </Stack>
+        </Card.Body>
+      </Card>
+    </>
+  )
+}
+
+function AlertCard() {
+  return (
+    <>
+      <Card bg="danger" className="d-none">
+        <Card.Body>
+          This is an alert
         </Card.Body>
       </Card>
     </>
@@ -41,7 +96,7 @@ function InputCard() {
 function StatusCard() {
   return (
     <>
-      <Card bg="info">
+      <Card className="bg-info bg-opacity-25">
         <Card.Body>
           System disabled
         </Card.Body>
@@ -58,6 +113,7 @@ function App() {
       <Stack gap="2">
         <InputCard />
         <StatusCard />
+        <AlertCard />
       </Stack>
     
     </Container>
